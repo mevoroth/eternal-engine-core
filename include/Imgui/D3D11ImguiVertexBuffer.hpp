363338fdc9c8f1cfb8b06f7468231d1801197d20
Daniel Quach <daniel@quach.info>
2021-06-24 01:37:20 +0200

    Unused file

--- include/Imgui/D3D11ImguiVertexBuffer.hpp
+++ include/Imgui/D3D11ImguiVertexBuffer.hpp
@@ -1,7 +1,7 @@
 #ifndef _D3D11_IMGUI_VERTEX_BUFFER_HPP_
 #define _D3D11_IMGUI_VERTEX_BUFFER_HPP_
 
-#include "d3d11/D3D11VertexBuffer.hpp"
+//#include "d3d11/D3D11VertexBuffer.hpp"
 
 #include "imgui.h"
 
@@ -9,10 +9,10 @@ namespace Eternal
 {
 	namespace Imgui
 	{
-		using namespace Eternal::Graphics;
-		using namespace std;
+		//using namespace Eternal::Graphics;
+		//using namespace std;
 
-		typedef D3D11VertexBuffer<ImDrawVert> D3D11ImguiVertexBuffer;
+		//typedef D3D11VertexBuffer<ImDrawVert> D3D11ImguiVertexBuffer;
 	}
 }
 
