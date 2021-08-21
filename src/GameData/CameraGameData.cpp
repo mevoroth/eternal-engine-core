d7216ce4d15f9775ac747736b70d02ad4ad82f1c
Daniel Quach <daniel@quach.info>
2021-08-21 23:32:42 +0200

    Camera game object

--- src/GameData/CameraGameData.cpp
+++ src/GameData/CameraGameData.cpp
@@ -1,18 +1,19 @@
 #include "GameData/CameraGameData.hpp"
 
-#include "Core/GameObject.hpp"
+#include "GameObjects/CameraGameObject.hpp"
 
 namespace Eternal
 {
 	namespace GameDataSystem
 	{
 		using namespace Eternal::Core;
+		using namespace Eternal::GameObjects;
 
 		void* CameraGameData::InternalLoad(_In_ const GameDataSource& InNode) const
 		{
-			GameObject* CameraGameObject = new GameObject();
-			InNode.Get<GameObject>(*CameraGameObject);
-			return CameraGameObject;
+			CameraGameObject* CameraObject = new CameraGameObject();
+			InNode.Get<GameObject>(*CameraObject);
+			return CameraObject;
 		}
 	}
 }
