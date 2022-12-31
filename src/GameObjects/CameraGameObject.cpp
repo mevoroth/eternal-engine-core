dda277f3ddc2e2e2caf24b08be77dd7d45ab323c
Daniel Quach <darkskana@gmail.com>
2022-12-31 02:34:45 +0100

    camera rotation + deadzone

--- src/GameObjects/CameraGameObject.cpp
+++ src/GameObjects/CameraGameObject.cpp
@@ -36,21 +36,19 @@ namespace Eternal
 			Vector3 ForwardVector(Forward.x, Forward.y, Forward.z);
 			Vector3 RightVector(Right.x, Right.y, Right.z);
 
-			float LX = InputSystem.GetAxis(Input::JOY0_LX);
-			float LY = InputSystem.GetAxis(Input::JOY0_LY);
-			float AbsLX = Abs(LX);
-			float AbsLY = Abs(LY);
-			float LXWithDeadZone = Sign(LX) * (AbsLX < 0.1f ? 0.0f : AbsLX);
-			float LYWithDeadZone = Sign(LY) * (AbsLY < 0.1f ? 0.0f : AbsLY);
+			float LXWithDeadZone = InputSystem.GetAxisWithDeadZone(Input::JOY0_LX);
+			float LYWithDeadZone = InputSystem.GetAxisWithDeadZone(Input::JOY0_LY);
+			float RXWithDeadZone = InputSystem.GetAxisWithDeadZone(Input::JOY0_RX);
+			float RYWithDeadZone = InputSystem.GetAxisWithDeadZone(Input::JOY0_RY);
 
 			Component->GetTransform().Translate(
 				RightVector * LXWithDeadZone +
 				ForwardVector * LYWithDeadZone
 			);
 
-			//Transform->GetTransform().Rotate(
-
-			//);
+			Component->GetTransform().Rotate(
+				Vector3(0.0f, RXWithDeadZone * 0.1f, RYWithDeadZone * 0.1f)
+			);
 		}
 
 		void CameraGameObject::UpdateDebug()
@@ -60,7 +58,8 @@ namespace Eternal
 			TransformComponent* Component = static_cast<TransformComponent*>(GetComponent<TransformComponent>());
 
 			ImGui::Begin("Camera");
-			ImGui::Text("Input X: [%.3f] / Input Y: [%.3f]", InputSystem.GetAxis(Input::JOY0_LX), InputSystem.GetAxis(Input::JOY0_LY));
+			ImGui::Text("Input X: [%.3f] / Input Y: [%.3f]", InputSystem.GetAxisWithDeadZone(Input::JOY0_LX), InputSystem.GetAxisWithDeadZone(Input::JOY0_LY));
+			ImGui::Text("Input RX: [%.3f] / Input RY: [%.3f]", InputSystem.GetAxisWithDeadZone(Input::JOY0_RX), InputSystem.GetAxisWithDeadZone(Input::JOY0_RY));
 			ImGui::InputFloat3("Camera Position:", &Component->GetTransform().GetTranslation().x);
 			ImGui::End();
 		}
