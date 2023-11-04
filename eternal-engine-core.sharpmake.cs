aaeccaf0abff695f13d23355605191ac4fcef07e
Daniel Quach <daniel@quach.info>
2023-11-04 15:54:08 +0000

    Core sharpmake

---
+++ eternal-engine-core.sharpmake.cs
@@ -0,0 +1,41 @@
+﻿using Sharpmake;
+
+[module: Sharpmake.Include(@"..\eternal-engine\eternal-engine.sharpmake.cs")]
+
+namespace EternalEngine
+{
+	[Sharpmake.Generate]
+	public class EternalEngineCoreProject : EternalEngineProject
+	{
+		public EternalEngineCoreProject()
+			: base(
+				"core",
+				new EternalEngineProjectSettings(
+					EternalEngineProjectSettingsFlags.EEPSF_IncludeSettingsHeader |
+					EternalEngineProjectSettingsFlags.EEPSF_IncludeHLSLReflection
+				)
+			)
+		{
+		}
+
+		public override void ConfigureAll(Configuration InConfiguration, Target InTarget)
+		{
+			base.ConfigureAll(InConfiguration, InTarget);
+
+			// Include paths
+			InConfiguration.IncludePaths.Add(new string[] {
+				@"$(SolutionDir)eternal-engine-components\include",
+				@"$(SolutionDir)eternal-engine-graphics\include",
+				@"$(SolutionDir)eternal-engine-extern\imgui",
+				@"$(SolutionDir)eternal-engine-extern\rapidjson\include",
+				@"$(SolutionDir)eternal-engine-extern\renderdoc",
+				@"$(SolutionDir)eternal-engine-shaders",
+				EternalEngineSettings.VulkanPath + @"\Include",
+				EternalEngineSettings.FBXSDKPath + @"\include",
+			});
+
+			InConfiguration.AddPublicDependency<EternalEngineExternProject>(InTarget);
+			InConfiguration.AddPublicDependency<EternalEngineUtilsProject>(InTarget);
+		}
+	}
+}
