3d6d5bae33c42e37f52bd7539a1b42dc72e24dde
Daniel Quach <daniel@quach.info>
2024-01-19 00:14:31 +0100

    Stencil Allocation

---
+++ include/GraphicsEngine/StencilAllocation.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace Eternal
+{
+	namespace GraphicsEngine
+	{
+		namespace StencilAllocation
+		{
+			static constexpr uint32_t OpaqueStencilBit = 0;
+			static constexpr uint32_t OpaqueStencil = (1 << OpaqueStencilBit);
+		}
+	}
+}
